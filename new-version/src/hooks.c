15558512ffcf74d3fbe0f3f8ea56d20e317e9069
Zineddaine Badr <57238132+Pedro-99@users.noreply.github.com>
2025-03-16 07:12:00 +0000

    remove movements player to move.c file

--- new-version/src/hooks.c
+++ new-version/src/hooks.c
@@ -2,40 +2,19 @@
 
 void ft_hook(void *param)
 {
-    t_data *data = (t_data *)param;
-    double move_speed = 5.0;
-    double rot_speed = 0.1;
+	t_data *data = (t_data *)param;
+	double move_speed = 5.0;
+	double rot_speed = 0.1;
 
-    if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
-        mlx_close_window(data->mlx);
-    if (mlx_is_key_down(data->mlx, MLX_KEY_W)) {
-        double new_x = data->player.x + cos(data->player.angle) * move_speed;
-        double new_y = data->player.y + sin(data->player.angle) * move_speed;
-        int map_x = (int)(new_x / 64);
-        int map_y = (int)(new_y / 64);
-        printf("x: %d\ty: %d\tmap_x: %d\tmap_y: %d\n", (int)new_x, (int)new_y, map_x, map_y);
-        if (map_x >= 0 && map_x < data->config.map_width && 
-            map_y >= 0 && map_y < data->config.map_height &&
-            data->config.map[map_y][map_x] != '1') {
-            data->player.x = new_x;
-            data->player.y = new_y;
-        }
-    }
-    if (mlx_is_key_down(data->mlx, MLX_KEY_S)) {
-        double new_x = data->player.x - cos(data->player.angle) * move_speed;
-        double new_y = data->player.y - sin(data->player.angle) * move_speed;
-        int map_x = (int)(new_x / 64);
-        int map_y = (int)(new_y / 64);
-        printf("x: %d\ty: %d\tmap_x: %d\tmap_y: %d\n", (int)new_x, (int)new_y, map_x, map_y);
-        if (map_x >= 0 && map_x < data->config.map_width && 
-            map_y >= 0 && map_y < data->config.map_height &&
-            data->config.map[map_y][map_x] != '1') {
-            data->player.x = new_x;
-            data->player.y = new_y;
-        }
-    }
-    if (mlx_is_key_down(data->mlx, MLX_KEY_A))
-        data->player.angle -= rot_speed;
-    if (mlx_is_key_down(data->mlx, MLX_KEY_D))
-        data->player.angle += rot_speed;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(data->mlx);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_W))
+		move_forward(data, move_speed);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_S)) {
+		move_backward(data, move_speed);
+	}
+	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
+		turn_left(data, rot_speed);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
+		turn_right(data, rot_speed);
 }
